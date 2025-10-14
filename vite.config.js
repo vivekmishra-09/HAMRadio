d76822afd4577c858cf1bf8183e4f70be72e93ca
Vivek Mishra <cmsindia012@gmail.com>
2025-10-14 10:11:25 +0530

    Fix rollup native module error for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  base: './', // ensures assets load correctly
+})
